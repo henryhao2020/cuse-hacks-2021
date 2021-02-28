0ab77086212a53a92b7917b169d52e736f2c5da5
Henry Hao <henryhao@berkeley.edu>
2021-02-28 01:45:06 -0800

    finishing project here. iterated through patient data and extracted zip codes

--- public/app.js
+++ public/app.js
@@ -22,11 +22,31 @@ const submitButton = document.getElementById("submitButton");
 
 var fileInput = document.getElementById("myfile"),
 
+// read the data from the Vaccine Provider
     readFile = function () {
         var reader = new FileReader();
         
         reader.onload = function () {
             document.getElementById('out').innerHTML = reader.result;
+            
+            // put the output of the csv file and separate them by commas. 
+            var listofstrings = reader.result.split(',');
+            var dict = {};
+            var i = 0;
+
+            // iterate through the list to grab all of the zipcodes
+            for (i = 0; i < listofstrings.length; i++) {
+              if (! isNaN(listofstrings[i])) {
+                //put those zipcodes into a dictionary where each key is the zipcode, and each value is the number of times the zipcode appears
+                dict[listofstrings[i]] = 1;
+              } 
+            }
+          // debugging
+            console.log(dict)
+
+            
+
+
         };
         // start reading the file. When it is done, calls the onload event defined above.
         reader.readAsBinaryString(fileInput.files[0]);
@@ -47,3 +67,5 @@ submitButton.addEventListener("click", function () {
 
 
 })
+
+
